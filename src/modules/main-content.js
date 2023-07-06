d2c99d0d10c5b4d2ead147b095600de9245e7900
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2023-07-06 13:33:32 -0300

    Add elements to main-content

--- src/modules/main-content.js
+++ src/modules/main-content.js
@@ -1,6 +1,12 @@
 const createMainContent = () => {
   const mainElement = document.createElement('main');
 
+  const navElement = document.createElement('nav');
+  mainElement.appendChild(navElement);
+
+  const taskContainer = document.createElement('section');
+  mainElement.appendChild(taskContainer);
+
   document.body.appendChild(mainElement);
 };
 
