57f15a1d4fd7c28b19480b8ef2460dc118264340
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2023-07-06 12:05:00 -0300

    Add logoContainer div

--- src/modules/header.js
+++ src/modules/header.js
@@ -3,16 +3,20 @@ import odinLogo from '../img/odin-lined.png';
 const createHeader = () => {
   const headerElement = document.createElement('header');
 
+  const logoContainer = document.createElement('div');
+  logoContainer.setAttribute('id', 'logo-container');
+  headerElement.appendChild(logoContainer);
+
   const logoImg = new Image();
   logoImg.src = odinLogo;
   logoImg.alt = 'The Odin Project logo';
   logoImg.setAttribute('id', 'odin-logo');
-  headerElement.appendChild(logoImg);
+  logoContainer.appendChild(logoImg);
 
   const logoText = document.createElement('p');
   logoText.setAttribute('id', 'logo-text');
   logoText.textContent = 'The Odin Project';
-  headerElement.appendChild(logoText);
+  logoContainer.appendChild(logoText);
 
   const headerText = document.createElement('p');
   headerText.textContent = 'Todo List';
