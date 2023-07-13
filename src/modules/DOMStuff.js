5d9bdd042655cbaf8e473ee78423d23a57b5e499
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2023-07-13 17:02:19 -0300

    Fix createNavButton function

--- src/modules/DOMStuff.js
+++ src/modules/DOMStuff.js
@@ -1,7 +1,8 @@
 const DOMStuff = (() => {
-  const createNavButton = (icon, text, parentNode) => {
+  const createNavButton = (icon, text, parentNode, id) => {
     const buttonElement = document.createElement('button');
     buttonElement.classList.add('nav-btn', 'font-20px', 'flex', 'align-center');
+    if (id) buttonElement.id = id;
 
     const spanElement = document.createElement('span');
     spanElement.textContent = icon;
@@ -13,11 +14,11 @@ const DOMStuff = (() => {
     parentNode.appendChild(buttonElement);
   };
 
-  const createNavListItem = (icon, text, parentNode) => {
+  const createNavListItem = (icon, text, parentNode, id) => {
     const listItem = document.createElement('li');
     listItem.classList.add('nav-item');
 
-    createNavButton(icon, text, listItem);
+    createNavButton(icon, text, listItem, id);
 
     parentNode.appendChild(listItem);
   };
@@ -37,6 +38,7 @@ const DOMStuff = (() => {
     newProjectName.id = 'projectName';
     newProjectName.name = 'projectName';
     newProjectName.required = true;
+    newProjectPopup.appendChild(newProjectName);
 
     const newProjectPopupButtons = document.createElement('div');
     newProjectPopupButtons.classList.add('flex', 'font-20px');
--- src/modules/nav.js
+++ src/modules/nav.js
@@ -3,18 +3,28 @@ import DOMStuff from './DOMStuff';
 const createNav = (parentNode) => {
   const navElement = document.createElement('nav');
 
-  const navList = document.createElement('ul');
-  navElement.appendChild(navList);
+  const navListTasks = document.createElement('ul');
+  navListTasks.setAttribute('id', 'nav-list-tasks');
+  navElement.appendChild(navListTasks);
 
-  DOMStuff.createNavListItem('home', 'Home', navList);
-  DOMStuff.createNavListItem('today', 'Today', navList);
-  DOMStuff.createNavListItem('date_range', 'Week', navList);
+  DOMStuff.createNavListItem('home', 'Home', navListTasks);
+  DOMStuff.createNavListItem('today', 'Today', navListTasks);
+  DOMStuff.createNavListItem('date_range', 'Week', navListTasks);
 
   const projectsTitle = document.createElement('h2');
   projectsTitle.textContent = 'Projects';
-  navList.appendChild(projectsTitle);
+  navElement.appendChild(projectsTitle);
 
-  DOMStuff.createNavListItem('add_circle', 'New project', navList);
+  const navListProjects = document.createElement('ul');
+  navListProjects.setAttribute('id', 'nav-list-projects');
+  navElement.appendChild(navListProjects);
+
+  DOMStuff.createNavListItem(
+    'add_circle',
+    'New project',
+    navListProjects,
+    'new-project-btn'
+  );
 
   parentNode.appendChild(navElement);
 };
