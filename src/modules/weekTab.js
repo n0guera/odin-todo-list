97d1828624593b37876693d4cf08b340bd90b317
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2023-08-21 10:35:37 -0300

    Add function to render week tab

--- src/modules/weekTab.js
+++ src/modules/weekTab.js
@@ -1,3 +1,20 @@
-const renderWeekTab = () => {};
+import todoStorage from './todoStorage';
+
+const renderWeekTab = () => {
+  const elementContainer = document.querySelector('#element-container');
+  elementContainer.innerHTML = '';
+
+  const pageTitle = document.createElement('h2');
+  pageTitle.id = 'page-title';
+  pageTitle.textContent = 'Week';
+  elementContainer.appendChild(pageTitle);
+
+  const taskContainer = document.createElement('div');
+  taskContainer.id = 'task-container';
+  elementContainer.appendChild(taskContainer);
+
+  if (todoStorage.getWeekTasks().length === 0)
+    taskContainer.style.display = 'none';
+};
 
 export default renderWeekTab;
